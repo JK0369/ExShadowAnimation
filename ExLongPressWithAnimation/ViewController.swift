9729ccb7f29168c1bac94cf5c33de8da5a7a3045
jake <palatable7@naver.com>
2023-07-09 21:51:38 +0900

    Implement

---
+++ ExLongPressWithAnimation/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  ExLongPressWithAnimation
+//
+//  Created by 김종권 on 2023/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let someView = UIView()
+    private var snapshotedView: UIView?
+    private var isDragging = false
+    private var originalPosition = CGPoint.zero
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        someView.backgroundColor = .green
+        view.addSubview(someView)
+        someView.frame = .init(x: 120, y: 120, width: 200, height: 200)
+        
+        let longPressGesture = UILongPressGestureRecognizer()
+        longPressGesture.minimumPressDuration = 0.3
+        longPressGesture.isEnabled = true
+        longPressGesture.delegate = self
+        longPressGesture.addTarget(self, action: #selector(handleLongPress))
+        
+        someView.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc private func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            handleBegan(gesture)
+        case .changed:
+            handleChanged(gesture)
+        default:
+            // ended, canceled, failed
+            handleEnded(gesture)
+        }
+    }
+    
+    private func handleBegan(_ gesture: UILongPressGestureRecognizer) {
+        originalPosition = gesture.location(in: view)
+        snapshotedView = someView.snapshotView(afterScreenUpdates: true)
+        snapshotedView?.frame = someView.frame
+        view.addSubview(snapshotedView!)
+        someView.alpha = 0
+    }
+    
+    private func handleChanged(_ gesture: UILongPressGestureRecognizer) {
+        let newLocation = gesture.location(in: view)
+        let xOffset = newLocation.x - originalPosition.x
+        let yOffset = newLocation.y - originalPosition.y
+        let translation = CGAffineTransform(translationX: xOffset, y: yOffset)
+        snapshotedView?.transform = translation
+    }
+    
+    private func handleEnded(_ gesture: UILongPressGestureRecognizer) {
+        snapshotedView?.alpha = 0
+        snapshotedView?.removeFromSuperview()
+        someView.alpha = 1
+    }
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        !isDragging
+    }
+}
