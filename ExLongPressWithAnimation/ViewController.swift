1a9775fd463fc9698817c4e9a916592b081aa887
jake <palatable7@naver.com>
2023-07-10 23:37:15 +0900

    Implement

--- ExLongPressWithAnimation/ViewController.swift
+++ ExLongPressWithAnimation/ViewController.swift
@@ -56,14 +56,46 @@ class ViewController: UIViewController {
         snapshotedView?.frame = someView.frame
         view.addSubview(snapshotedView!)
         someView.alpha = 0
+        
+        // Animation - 다른 뷰는 줄어들게하고, 이동시킬 뷰는 크게하기
+        UIView.animate(
+            withDuration: 0.4,
+            delay: 0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0,
+            options: [.allowUserInteraction, .beginFromCurrentState],
+            animations: { self.prepareDragAnimation() },
+            completion: nil
+        )
+    }
+
+    private func prepareDragAnimation() {
+        let upScale = 1.2
+        let scaleTranasform = CGAffineTransform(scaleX: upScale, y: upScale)
+        
+        let downYPosition = 10.0
+        let translationTransform = CGAffineTransform(translationX: 0, y: downYPosition)
+        snapshotedView?.transform = scaleTranasform.concatenating(translationTransform)
+        
+        snapshotedView?.alpha = 0.9
+
+        let downScale = 0.8
+        [otherView, anotherView]
+            .forEach { subview in
+                subview.transform = CGAffineTransform(scaleX: downScale, y: downScale)
+            }
     }
 
     private func handleChanged(_ gesture: UILongPressGestureRecognizer) {
         let newLocation = gesture.location(in: view)
         let xOffset = newLocation.x - originalPosition.x
         let yOffset = newLocation.y - originalPosition.y
-        let translation = CGAffineTransform(translationX: xOffset, y: yOffset)
-        snapshotedView?.transform = translation
+        let translationTransform = CGAffineTransform(translationX: xOffset, y: yOffset)
+        
+        // Animation - 업스케일 된 이동 시킬 뷰를 계속 업스케일된 상태로 유지하기
+        let upScale = 1.2
+        let scaleTranasform = CGAffineTransform(scaleX: upScale, y: upScale)
+        snapshotedView?.transform = translationTransform.concatenating(scaleTranasform)
     }
 
     private func handleEnded(_ gesture: UILongPressGestureRecognizer) {
